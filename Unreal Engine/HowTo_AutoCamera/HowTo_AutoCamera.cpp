89c4d361af48a7846c27a56ddb157bfdb7c9b511
Steven Hiltrop <24697316+stevenhiltrop@users.noreply.github.com>
2020-04-01 12:55:50 +0700

    Added Auto Camera
    Learned to auto pivot between 2 camera's automatically through C++

---
+++ Unreal Engine/HowTo_AutoCamera/HowTo_AutoCamera.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "HowTo_AutoCamera.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HowTo_AutoCamera, "HowTo_AutoCamera" );
