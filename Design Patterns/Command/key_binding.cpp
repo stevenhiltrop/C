7e35d46e5010d66a80fc0dd36b294851ed9990e3
Steven Hiltrop <24697316+stevenhiltrop@users.noreply.github.com>
2020-04-04 17:13:08 +0700

    Changed: key_binding.cpp
    It's now possible to undo the given command and restoring previous position.

--- Design Patterns/Command/key_binding.cpp
+++ Design Patterns/Command/key_binding.cpp
@@ -2,6 +2,7 @@ Class Command {
 public:
   virtual ~Command () {}
   virtual void execute(GameActor& actor) = 0;
+  virtual void undo() = 0;
 };
 
 Class JumpCommand : Command {
@@ -32,6 +33,27 @@ public:
   }
 };
 
+Command* handleInput()
+{
+  Unit* unit = getSelectedUnit();
+  
+  if (isPressed(BUTTON_UP)) {
+    // Move the unit up one.
+    int destY = unit->y() - 1;
+    return new MoveUnitCommand(unit, unit->x(), destY);
+  }
+  
+  if (isPressed(BUTTON_DOWN)) {
+    // Move the unit down one.
+    int destY = unit->y() + 1;
+    return new MoveUnitCommand(unit, unit->x(), destY);
+  }
+  
+  // Other moves...
+  
+  return NULL;
+};
+
 Class InputHandler {
 public:
   void handleInput();
@@ -43,6 +65,32 @@ private:
   Command* buttonY_;
   Command* buttonA_;
   Command* buttonB_;
+};
+
+class MoveUnitCommand : public Command
+{
+public:
+  MoveUnitCommand(Unit* unit, int x, int y):
+  unit_(unit),
+  xBefore_(0),
+  yBefore_(0),
+  x_(x),
+  y_(y)
+  {}
+  
+  virtual void execute()
+  {
+    // Remember the unit's position before the move
+    // so we can restore it.
+    xBefore_ = unit_->x();
+    yBefore_ = unit_->y();
+    unit_->moveTo(x_, y_);
+  }
+  
+private:
+  Unit* unit_;
+  int xBefore_, yBefore_;
+  int x_, y_;
 };
 
 int main( int argc, const char* argv[] ) {
