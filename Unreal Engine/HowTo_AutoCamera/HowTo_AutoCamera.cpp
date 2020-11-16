93864b6b811524b80a8535670b9924ad48915b3e
stevenhiltrop <stevenhiltrop@gmail.com>
2020-11-16 17:01:28 +0700

    Deleted Unreal Engine as it is moved to its own folder.

--- Unreal Engine/HowTo_AutoCamera/HowTo_AutoCamera.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
-
-#include "HowTo_AutoCamera.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HowTo_AutoCamera, "HowTo_AutoCamera" );
